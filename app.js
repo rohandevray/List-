a74d50207dffe5180fed20a198dc6ff315fdd025
Rohan Dev Ray <jessepinkman5656@gmail.com>
2022-03-23 01:04:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+let items = ["Buy the Food", "Cook the Food", "Eat the Food"]; //making an array to hold the list of todos
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleString("en-US", options);
+
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  //the data we have to grab is given a name which acts like the variable that contains the data
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/"); //it redirect to the home route so that we get the item we get inside the post method
+});
+
+app.listen(3000, function () {
+  console.log("Server started at port 3000");
+});
